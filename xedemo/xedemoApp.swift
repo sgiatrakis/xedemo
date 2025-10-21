1b0904c6ebf0c3d5bd397869bbf46dee5841e572
Lysimachos Giatrakis <sgiatrakis@gmail.com>
2025-10-21 14:10:15 +0300

    init ios project

---
+++ xedemo/xedemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  xedemoApp.swift
+//  xedemo
+//
+//  Created by Lysimachos Giatrakis on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct xedemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
